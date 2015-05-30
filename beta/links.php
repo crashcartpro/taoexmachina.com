67b42adf0db5900c641fa07fc66875397425fc9e
crashcartpro <crashcartpro@hotmail.com>
2015-05-29 22:03:48 -0500

    playing with sticky nav bar thing

--- beta/links.php
+++ beta/links.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 <title>The Way from a machine</title>
-<base href="http://www.taoexmachina.com/">
+<base href="http://www.taoexmachina.com/beta">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="maroon-black-ivory-07-2014.css">
+<link rel="stylesheet" type="text/css" href="beta/maroon-black-ivory-02-2015.css">
 </head>
 <body>
 <?php
@@ -12,10 +12,13 @@
 /*===== PULL HEADERS =====*/
 include 'header.part';
 include 'nav-bar.part';
+/*===== PAGE CONTENT =====*/
+echo "<div id='content'>";
+echo "<div class='wrapper'>";
 include 'quote.part';
 
 ?>
-<div class="content">
+<div class="article">
 <h2>Tim around the web</h2>
 <hr>
 https://www.facebook.com/insideoutboy<br/>
@@ -23,6 +26,8 @@ https://twitter.com/crashcartpro<br/>
 https://256.makerslocal.org/wiki/User:CrashCart<br/>
 https://github.com/crashcartpro<br/>
 http://www.codecademy.com/crashcartpro<br/>
+</div>
+</div>
 </div>
 <?php
 
--- beta/museum.php
+++ beta/museum.php
@@ -4,38 +4,43 @@
 <title>The Way from a machine</title>
 <base href="http://www.taoexmachina.com/">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="maroon-black-ivory-07-2014.css">
+<link rel="stylesheet" type="text/css" href="beta/maroon-black-ivory-02-2015.css">
 </head>
 <body>
 <?php
 
 /*===== PULL HEADERS =====*/
 include 'header.part';
-include 'navigation.part';
+include 'nav-bar.part';
+/*===== PAGE CONTENT =====*/
+echo "<div id='content'>";
+echo "<div class='wrapper'>";
 include 'quote.part';
 
 ?>
-<div class="content">
+<div class="article">
 <h2>Hardware</h2>
 <hr>
 </div>
 
-<div class="content">
+<div class="article">
 <h2>Software</h2>
 <hr>
 
 </div>
 
-<div class="content">
+<div class="article">
 <h2>Images</h2>
 <hr>
 
 </div>
 
-<div class="content">
+<div class="article">
 <h2>Music</h2>
 <hr>
 
+</div>
+</div>
 </div>
 <?php
 
