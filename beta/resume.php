31f0a4ddf7c5ff089244fa90976e82af2fb91dfe
crashcartpro <crashcartpro@hotmail.com>
2015-05-30 21:49:26 -0500

    jquery sticky nav pushed to minor pages in beta

--- beta/resume.php
+++ beta/resume.php
@@ -5,6 +5,33 @@
 <base href="http://www.taoexmachina.com/beta">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="beta/maroon-black-ivory-02-2015.css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript">
+        $(document).ready(function() {
+                // grab the initial top offset of the navigation 
+                var stickyNavTop = $('.nav-bar').offset().top;
+                console.log(stickyNavTop);
+                // our function that decides weather the navigation bar should have "fixed" css position or not.
+                var stickyNav = function(){
+                    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+                    console.log(scrollTop);     
+                    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+                    // otherwise change it back to relative
+                    if (scrollTop > stickyNavTop) { 
+                        $('.nav-bar').addClass('sticky');
+                        console.log("add class");
+                    } else {
+                        $('.nav-bar').removeClass('sticky'); 
+                    }
+                };
+
+                stickyNav();
+                // and run it again every time you scroll
+                $(window).scroll(function() {
+                        stickyNav();
+                });
+        });
+</script>
 </head>
 <body>
 <?php
