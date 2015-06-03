173530a9d4eb1e4c62319dc82033734f99800c59
crashcartpro <crashcartpro@hotmail.com>
2015-06-03 00:21:06 -0500

    updated resume section, removed more old files.

--- resume.php
+++ resume.php
@@ -27,48 +27,66 @@ include 'quote.part';
 <div class="article">
 <h2><a id="education">Education and Certification</a></h2>
 <hr>
-<p><h3>Computer Service and Repair</h3>
-2001 - Huot Technical Center, Laconia, NH</p>
+<p><h3>L2: cPanel Professional Certification: Technical</h3>
+2015 - university.cpanel.net
+
+<p><h3>Associate of Applied Science</h3>
+2009-2013 - Attended Calhoun Community College, Huntsville, AL</p>
 
 <p><h3>Digium Certified Asterisk Profesional (dCAP)</h3>
 2010 - Huntsvile, AL</p>
 
-<p><h3>Associate of Applied Science</h3>
-2009-2013 - Attended Calhoun Community College, Huntsville, AL</p>
+<p><h3>Computer Service and Repair</h3>
+2001 - Huot Technical Center, Laconia, NH</p>
+
 </div>
 
 <div class="article">
 <h2><a id="work">Work History</a></h2>
 <hr>
-<p><h3>Technology Coordinator's Assistant</h3>
-May 2002-June2002<br>
-Franklin School District, Franklin NH
-Clean and repair computers and computer labs in preparation for the next school year.</p>
+<p><h3>Technical Support Technician</h3>
+July 2014-Present
+Simple Helix, Huntsville, AL
+Troubleshooting GNU/Linux based web hosting. Answering customer questions and concerns regarding products</p>
 
-<p><h3>Freelance Computer Repair</h3>
-July 2002-2008<br>
-Built, setup, and repaired home computers and networks.</p>
+<p><h3>Logistics Technical Specialist</h3>
+April 2014-July 2014
+Digium Inc. Huntsville, AL
+Perform failure analysis on returned items including: phones, gateways, PC cards, and servers. Flag repeat issues to investigate possible quality assurance or hardware design issues.</p>
 
-<p><h3>Delivery Driver</h3>
-June 2005-December 2007<br>
-NeighborCare, Pembroke NH
-Preparation, printing, and sorting of delivery manifests as well as the subsequent delivery of pharmaceuticals and med-supplies.</p>
+<p><h3>Warehouse Technician</h3>
+January 2014-April 2014
+Wide Open West, Huntsville, AL
+Update firmware on digital transport adapters (DTA). Planned and documented DTA equipment testing procedures. Assists with additional warehouse operations as needed </p>
 
-<p><h3>Computer Consultant and Repair Tech</h3>
-September 2006-January 2007<br>
-Franklin Career Academy, Franklin NH
-Maintenance and repairs on school computers, server, and networks (wired and wireless) As well as troubleshooting school software and NWEA testing programs.</p>
+<p><h3>Technical Support Technician</h3>
+March 2010-June 2013<br>
+Digium Inc. Huntsville, AL
+Providing technical assistance to Digium customers over phone and email. Maintaining Asterisk and Switchvox communication systems. Troubleshooting VoIP, PRI, and analog telephony connections.</p>
 
 <p><h3>Pharmacy Technician</h3>
 May 2008-July 2009<br>
 Sunscript, Huntsville, AL
 Filling prescriptions. Sorting/counting pills. Preparation, printing, and sorting of delivery totes and manifests.</p>
 
-<p><h3>Technical Support Technician</h3> 
-March 2010-June 2013<br>
-Digium Inc. Huntsville, AL
-Providing technical assistance to Digium customers over phone and email. Maintaining Asterisk and Switchvox communication systems. Troubleshooting VoIP, PRI, and analog telephony connections.</p>
+<p><h3>Computer Consultant and Repair Tech</h3>
+September 2006-January 2007<br>
+Franklin Career Academy, Franklin NH
+Maintenance and repairs on school computers, server, and networks (wired and wireless) As well as troubleshooting school software and NWEA testing programs.</p>
 
+<p><h3>Delivery Driver</h3>
+June 2005-December 2007<br>
+NeighborCare, Pembroke NH
+Preparation, printing, and sorting of delivery manifests as well as the subsequent delivery of pharmaceuticals and med-supplies.</p>
+
+<p><h3>Freelance Computer Repair</h3>
+July 2002-2008<br>
+Built, setup, and repaired home computers and networks.</p>
+
+<p><h3>Technology Coordinator's Assistant</h3>
+May 2002-June2002<br>
+Franklin School District, Franklin NH
+Clean and repair computers and computer labs in preparation for the next school year.</p>
 </div>
 </div>
 </div>
